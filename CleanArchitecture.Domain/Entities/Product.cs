1f27160aa20edbc7654f8d31a4d5d1376c1bbb96
Wandy Rodríguez <wandyrh@hotmail.com>
2025-05-24 21:25:57 -0400

    add domain entities and interfaces (#1)

---
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ProductCategory Category { get; set; }
+}
---
+++ CleanArchitecture.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace CleanArchitecture.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+
+    Task UpdateAsync(T entity);
+    Task UpdateRangeAsync(IEnumerable<T> entities);
+
+    Task RemoveAsync(T entity);
+    Task RemoveRangeAsync(IEnumerable<T> entities);
+
+    Task<bool> ExistsAsync(Guid id);
+    Task<int> CountAsync();
+}
