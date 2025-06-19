ab116ba47e687206fdb570845e2deb8d1740b3e2
Wandy Rodríguez <wandyrh@hotmail.com>
2025-06-18 23:09:37 -0400

    Release4 (#30)
    * add domain entities and interfaces (#1)
    
    * add repositories implementation (#2)
    
    add unit of work
    register DI
    
    * add service implementation (#3)
    
    * add controller implementation (#4)
    
    add pagination
    
    * add fluent validation (#6)
    
    add GlobalExceptionHandlerMiddleware
    
    * add UpdateProductDtoValidator (#7)
    
    * fix DI of fluent validation (#9)
    
    * add products crud (#10)
    
    * implement soft delete (#11)
    
    * add custom exceptions and entity validation (#12)
    
    * add security (#13)
    
    protect endpoints
    add login endpoint
    
    * add Product Category CRUD (#14)
    
    * add full audit to operations (#15)
    
    * move log and error messages to static classes (#16)
    
    * update README.md information (#17)
    
    * Feature/add unit tests (#19)
    
    * add application layer unit tests
    
    * add unit tests to web api layer
    
    * add Infrastructure unit tests
    
    * add docker-compose support (#20)
    
    * implement in memory cache (#21)
    
    * Add Health Checks (#22)
    
    * return CategoryName in ProductDto (#23)
    
    * Update documentation (#25)
    
    * Feature/update readme (#27)
    
    * Update documentation
    
    * add sql project on GitHub
    
    * Fix/improve error response (#29)
    
    * Update documentation
    
    * add sql project on GitHub
    
    * improve error response

--- CleanArchitecture.Domain/Interfaces/IUserRepository.cs
+++ CleanArchitecture.Domain/Interfaces/IUserRepository.cs
@@ -4,5 +4,5 @@ namespace CleanArchitecture.Domain.Interfaces;
 
 public interface IUserRepository : IGenericRepository<User>
 {
-    Task<User> GetUserByEmailAsync(string email);
+    Task<User?> GetUserByEmailAsync(string email);
 }
